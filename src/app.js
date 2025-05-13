153d967fc51f27510a841c7374940e5e43d898b0
Minal Panchal <minal.panchal8603@gmail.com>
2025-05-13 22:57:46 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from "express"
-
 import cors from "cors"
 import cookieParser from "cookie-parser"
+
+
 const app=express()
 
 app.use(cors({
@@ -12,4 +13,14 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+
 export{app}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
@@ -5,10 +6,12 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path:'./env'
 })
+
+const app=express();
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is running at port:$ {process.env.PORT}`)
+        console.log(`Server is running at port:${process.env.PORT}`)
     })
 })
 .catch((err)=>{
